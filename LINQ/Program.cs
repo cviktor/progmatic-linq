5d8099f6c4b9de602892b85f0f233d9206176224
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-07-19 14:25:35 +0200

    LINQ Select példa

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -12,13 +12,19 @@ namespace LINQ
         {
             List<int> numbers = new List<int> { 2, 3, 4, 5, 6, 7, 8, 9 };
 
-            IEnumerable<int> evens = numbers.Where((n) => n % 2 == 0);
-            var evenAndBiggerThan4 = evens.Where((n) => n > 4);
-            foreach (var number in evenAndBiggerThan4)
-            {
-                Console.WriteLine(number);
-            }
+            //IEnumerable<int> evens = numbers.Where((n) => n % 2 == 0);
+            //var evenAndBiggerThan4 = evens.Where((n) => n > 4);
+            //var evenAndBiggerThan4 = numbers.Where((n) => n % 2 == 0).Where((n) => n > 4);
+            //foreach (var number in evenAndBiggerThan4)
+            //{
+            //    Console.WriteLine(number);
+            //}
 
+            IEnumerable<string> stringNumbers = numbers.Select((n) => n + " szám");
+            foreach (string numberText in stringNumbers)
+            {
+                Console.WriteLine(numberText);
+            }
 
             Console.ReadKey();
         }
