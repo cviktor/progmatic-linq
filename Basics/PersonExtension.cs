f6c098d73bf76c35723254f1844e629870fcb9af
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-07-19 09:53:55 +0200

    Extension method példa

---
+++ Basics/PersonExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    static class PersonExtension
+    {
+        public static void Write(this Person p)
+        {
+            Console.WriteLine(p.Name + " fizetése: " + p.Salary);
+        }
+    }
+}
--- Basics/Program.cs
+++ Basics/Program.cs
@@ -11,7 +11,7 @@ namespace Basics
         static void Main(string[] args)
         {
             var x = 2;
-            var line = Console.ReadLine(); //metódus visszatérési érték tárolására nem mindig preferálják, van hogy inkább ki kell írni
+            //var line = Console.ReadLine(); //metódus visszatérési érték tárolására nem mindig preferálják, van hogy inkább ki kell írni
 
             //var p = new Person("Lajos");
             //p.Age = 31;
@@ -22,16 +22,47 @@ namespace Basics
                 Salary = 1000
             };
 
+            var p2 = new Person("Armando")
+            {
+                Age = 27,
+                Salary = 2000
+            };
+
+
             //var persons = new List<Person>();
             //persons.Add(p);
+            //persons.Add(new Person("Géza"){Age = 22, Salary = 3000});
 
             var persons = new List<Person>()
             {
                 p,
                 new Person("Géza"){Age = 22, Salary = 3000}
             };
+            Console.WriteLine(persons[1].Name);
+
+            //Console.WriteLine(p.Name + " fizetés: " + p.Salary);
+            //Console.WriteLine(p2.Name + " fizetés: " + p2.Salary);
+            //Console.WriteLine(persons[1].Name + " fizetés: " + persons[1].Salary);
+            //WritePerson(p);
+            //WritePerson(p2);
+            //WritePerson(persons[1]);
+
+            //Megegyezik az alsóval
+            PersonExtension.Write(p);
+            PersonExtension.Write(p2);
+            PersonExtension.Write(persons[1]);
+            
+            //Extension method hívása
+            p.Write();
+            p2.Write();
+            persons[1].Write();
 
             Console.ReadKey();
+        }
+
+        static void WritePerson(Person p)
+        {
+            Console.WriteLine(p.Name + " fizetés: " + p.Salary);
         }
 
         static int Y()
